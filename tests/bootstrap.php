4c465ed232d1eb464cab7c72c8e880dda8e844f1
Gregory K <gregory@tiv.net>
2014-03-17 18:04:57 -0400

    Meet Travis-CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,24 @@
 <?php
-/**
- * @link   https://github.com/wp-cli/sample-plugin/blob/master/tests/bootstrap.php
- * @author scribu
- */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
+echo "Welcome to the TIVWP Test Suite" . PHP_EOL;
+echo "Version: 1.0" . PHP_EOL . PHP_EOL;
 
-require_once $_tests_dir . '/includes/functions.php';
+// load some helpful functions
+require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
+// Activates this plugin in WordPress so it can be tested.
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../tivwp-dm.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php';
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( 'tivwp-dm/tivwp-dm.php' ),
+);
+
+// fire up test suite
+require_once getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+
+// include unit test base class
+//require_once dirname( __FILE__ ) . '/framework/class-wpseo-unit-test-case.php';
