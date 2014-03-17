8d224f04cd5adbf13e1fc5beaed1df33132ed3f3
Gregory K <gregory@tiv.net>
2014-03-17 17:43:37 -0400

    Meet Travis-CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,20 @@
 <?php
 /**
- *
+ * @link   https://github.com/wp-cli/sample-plugin/blob/master/tests/bootstrap.php
+ * @author scribu
  */
-$_tests_dir = getenv('WP_TESTS_DIR');
-#if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-#require_once $_tests_dir . '/includes/functions.php';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../tivwp-dm.php';
 }
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-//require $_tests_dir . '/includes/bootstrap.php';
+require $_tests_dir . '/includes/bootstrap.php';
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,6 +1,8 @@
 <?php
 /**
- *
+ * Sample PHPUnit test
+ * @link   https://github.com/wp-cli/sample-plugin/blob/master/tests/test-sample.php
+ * @author scribu
  */
 
 class SampleTest extends WP_UnitTestCase {
