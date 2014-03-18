324ecd139eef962bbe379d5ba426cb72a11811d1
Gregory K <gregory@tiv.net>
2014-03-17 20:17:21 -0400

    Meet Travis-CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ echo "Welcome to the TIVWP Test Suite" . PHP_EOL;
 echo "Version: 1.0" . PHP_EOL . PHP_EOL;
 
 // load some helpful functions
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+require_once getenv( 'WP_TESTS_DIR' ) . 'functions.php';
 
 // Activates this plugin in WordPress so it can be tested.
 function _manually_load_plugin() {
@@ -18,7 +18,7 @@ $GLOBALS['wp_tests_options'] = array(
 );
 
 // fire up test suite
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+require_once getenv( 'WP_TESTS_DIR' ) . 'bootstrap.php';
 
 // include unit test base class
 //require_once dirname( __FILE__ ) . '/framework/class-wpseo-unit-test-case.php';
